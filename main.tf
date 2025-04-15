92301eb621d3f4a35c95757aefd73f9680699b32
nithin <nithin.yadla@bilvantis.io>
2025-04-15 17:20:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
-
+variable "SERVICE_ACCOUNT_KEY" {
+  description = "GCP service account key JSON content"
+  type        = string
+  sensitive   = true
+}
 
 provider "google" {
   project     = "bilvantisaimlproject"
   region      = "asia-south1"
   zone        = "asia-south1-a"
-  credentials = file("credentials_file.json")
+  credentials = var.SERVICE_ACCOUNT_KEY
 }
 
 resource "google_compute_instance" "vm_instance" {
